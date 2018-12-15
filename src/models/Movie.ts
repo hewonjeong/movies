03417b4ff9983a1f64459c3c453c03368ea47a7a
Hewon Jeong <hewon.jng@gmail.com>
2018-12-15 14:07:02 +0900

    add scan method to Movie model

--- src/models/Movie.ts
+++ src/models/Movie.ts
@@ -1,6 +1,7 @@
-import awsSdk from 'aws-sdk'
+import awsSdk, { ResourceGroupsTaggingAPI } from 'aws-sdk'
 import { aws } from '../config/constans'
 import { Movie as MovieType } from '../types'
+import { ScanInput, AttributeMap, Key } from 'aws-sdk/clients/dynamodb'
 
 export default class Movie {
   static client = new awsSdk.DynamoDB.DocumentClient(aws)
@@ -25,4 +26,75 @@ export default class Movie {
     }
     return await Movie.client.batchWrite(params).promise()
   }
+
+  static async scan(scanner: Scanner) {
+    let res: AttributeMap[] = []
+    let exclusiveStartKey = undefined
+    do {
+      const params = getParams(scanner, exclusiveStartKey)
+      const { Items = [], LastEvaluatedKey = {} } = await Movie.client
+        .scan(params)
+        .promise()
+      res = res.concat(Items)
+      exclusiveStartKey = LastEvaluatedKey.key
+    } while (exclusiveStartKey)
+
+    return res
+  }
+}
+
+const getAttributes = (scanner: Scanner) => {
+  const { minYear, maxYear, title, rating, genre } = scanner
+  return [
+    !!title && 'title',
+    !!(minYear || maxYear) && 'year',
+    !!rating && 'rating',
+    !!genre && 'genre',
+  ]
+    .filter(Boolean)
+    .reduce((acc, cur) => {
+      return Object.assign({}, acc, cur && { [`#${cur}`]: cur })
+    }, {})
+}
+const getParams = (scanner: Scanner, exclusiveStartKey?: string) => {
+  const { minYear, maxYear, title, rating, genre } = scanner
+  const { tomatoScore, popcornScore } = scanner
+
+  const expression = [
+    title && 'contains (#title, :title)',
+    minYear && '#year >= :minYear',
+    maxYear && '#year <= :maxYear',
+  ]
+    .filter(Boolean)
+    .join(' and ')
+
+  const attributes = getAttributes(scanner)
+
+  return {
+    TableName: 'movies',
+    FilterExpression: expression,
+    ExpressionAttributeNames: attributes,
+    ExpressionAttributeValues: {
+      ':minYear': minYear,
+      ':maxYear': maxYear,
+      ':title': title,
+      // TODO: add more ExpressionAttributeValues
+      // ':tomatoScore': tomatoScore,
+      // ':popcornScore': popcornScore,
+      // ':rating': rating,
+      // ...genreValues,
+    },
+    ExclusiveStartKey: exclusiveStartKey
+      ? { key: exclusiveStartKey }
+      : undefined,
+  }
+}
+interface Scanner {
+  minYear?: number
+  maxYear?: number
+  title?: string
+  tomatoScore?: number
+  popcornScore?: number
+  rating?: string
+  genre?: number[]
 }

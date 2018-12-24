a10fd080c72cfa7eb683c5118fa9446056da6a0f
Hewon Jeong <hewon.jng@gmail.com>
2018-12-24 21:42:06 +0900

    add Award model

---
+++ src/models/Award.ts
@@ -0,0 +1,28 @@
+import awsSdk from 'aws-sdk'
+import { aws } from '../config/constans'
+import { Award as AwardType } from '../scrapper/Awards'
+
+export default class Award {
+  static client = new awsSdk.DynamoDB.DocumentClient(aws)
+  static TABLE = 'awards'
+
+  static async create(award: AwardType) {
+    const params = {
+      TableName: Award.TABLE,
+      Item: award,
+    }
+    await Award.client.put(params).promise()
+    return award
+  }
+
+  static async batch(awards: AwardType[]) {
+    const params = {
+      RequestItems: {
+        [Award.TABLE]: awards.map(award => ({
+          PutRequest: { Item: award },
+        })),
+      },
+    }
+    return await Award.client.batchWrite(params).promise()
+  }
+}

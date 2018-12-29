82d2f024b5708a4997b106d785a04ccc35c43f08
Hewon Jeong <hewon.jng@gmail.com>
2018-12-30 01:20:01 +0900

    pause and resume when read a line with stream

--- src/utils/file.ts
+++ src/utils/file.ts
@@ -1,12 +1,18 @@
 import fs from 'fs'
 import readline from 'readline'
 
-export const readFile = (file: string, onLine: (line: string) => void) => {
+export const readFile = (
+  file: string,
+  onLine: (line: string, cb: () => void) => void
+) => {
   const instream = fs.createReadStream(file)
 
   const rl = readline.createInterface({ input: instream })
 
-  rl.on('line', onLine)
+  rl.on('line', line => {
+    rl.pause()
+    onLine(line, () => rl.resume())
+  })
 }
 
 const writeFile = (file: string) => {

972085d93e7cb973b5ce6e08b4a167c42d40237b
Hewon Jeong <hewon.jng@gmail.com>
2019-11-27 15:38:37 +0900

    chore: move httpsAgent into getDocument function

--- src/getDocument.ts
+++ src/getDocument.ts
@@ -2,11 +2,9 @@ import axios from 'axios'
 
 const https = require('https')
 
-const httpsAgent = new https.Agent({
-  rejectUnauthorized: false,
-})
-
 export default async (url: string) => {
+  const httpsAgent = new https.Agent({ rejectUnauthorized: false })
   const response = await axios.get<string>(url, { httpsAgent })
+
   return response.data
 }
